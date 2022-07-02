0cbf2f804f57c52b1895fcb832aec156a55d975f
unknown <truongtansang61@gmail.com>
2022-07-02 23:41:50 +0700

    upload new source code

--- assets/code/code.js
+++ assets/code/code.js
@@ -130,80 +130,125 @@ var sidebar_left = document.querySelector('.js-sidebar_left');
     })
 
 //  menu search START ================================================================================================
-
+;( function(){
 var menu_bar_search = document.querySelector('.js-menubar_search');
-var menu_bar_items = menu_bar_search.querySelectorAll('.search_item');
 var menu_bar_input = menu_bar_search.querySelector('.js-menu_search')
+var menu_bar_blocks = menu_bar_search.querySelectorAll('.search_block');
 
 
-var list_items_search = [];
+var list_blocks_search = [];
 
-menu_bar_items.forEach(function(value,index){
-    list_items_search.push(removeVietnameseTones(value.querySelector('.mypage_content p').innerText.toLowerCase()));
+menu_bar_blocks.forEach(function(value,index){
+    var modal = [];
+    var items = value.querySelectorAll(".search_item");
+    items.forEach(function(item_value,item_index){
+        modal.push(removeVietnameseTones(item_value.querySelector('.mypage_content p').innerText.toLowerCase()));
+    })
+    list_blocks_search.push(modal);
 })
 
 menu_bar_input.addEventListener('keyup',function(){
-    var val_input = removeVietnameseTones(menu_bar_input.value.toLowerCase());
-    
-    list_items_search.forEach(function(value,index){ 
-        var value = list_items_search[index];
+    var val_input = removeVietnameseTones(menu_bar_input.value.toLowerCase());  
 
-        //  var a = menu_bar_items[index].classList.add('ds0');
-        //  var b = menu_bar_items[index].parentNode.parentNode.parentNode.querySelector('p').classList.add('ds0');   // từ thẻ a -> thẻ li -> ul -> div
-        //  var c = menu_bar_items[index].parentNode.parentNode.classList.remove('boder_bottom');
+    list_blocks_search.forEach(function(value,index){ 
 
+        menu_bar_blocks[index].classList.add("ds0");
+        var list_items = menu_bar_blocks[index].querySelectorAll(".search_item");
         
-        var a = menu_bar_items[index];
-        var b = menu_bar_items[index].parentNode.parentNode.parentNode.querySelector('p');   // từ thẻ a -> thẻ li -> ul -> div
-        var c = menu_bar_items[index].parentNode.parentNode;
+        value.forEach(function(text_value,text_index){
+            list_items[text_index].classList.remove("ds0");
 
-        a.classList.add('ds0');
-        b.classList.add('ds0');
-        c.classList.remove('boder_bottom');
-
-
-        if(value.indexOf(val_input) !== -1){
-            a.classList.remove('ds0');
-            b.classList.remove('ds0');
-            c.classList.add('boder_bottom');
-            
-            console.log(a,b,c);
-            // menu_bar_items[index].classList.remove('ds0');
-            // menu_bar_items[index].parentNode.parentNode.parentNode.querySelector('p').classList.remove('ds0');  
-            // menu_bar_items[index].parentNode.parentNode.classList.add('boder_bottom');
-        }
-            
-            
-        });    
-});
-//  menu search END ================================================================================================
-
-
-//  chat search START ================================================================================================
-
-var menu_chat = document.querySelector('.js-list_chat');
-var list_chat_items = menu_chat.querySelectorAll('.messenger');
-var chat_input = menu_chat.querySelector('.js-chat_search')
-
-var list_chat_item_search = [];
-
-list_chat_items.forEach(function(value,index){
-    list_chat_item_search.push(removeVietnameseTones(value.querySelector('.messenger_content p').innerText.toLowerCase()));
-});
-
-
-chat_input.addEventListener('keyup',function(){
-    var val_input = removeVietnameseTones(chat_input.value.toLowerCase());
-
-    list_chat_item_search.forEach(function(value,index){
-
-        list_chat_items[index].parentNode.classList.add('ds0');
-
-        if(value.indexOf(val_input) !== -1){
-            list_chat_items[index].parentNode.classList.remove('ds0');
-        }
+            if(text_value.indexOf(val_input) !== -1){
+                menu_bar_blocks[index].classList.remove("ds0");                
+            }
+            else{
+                list_items[text_index].classList.add("ds0"); 
+            }
+        })
     });    
 });
+})();
+//  menu search END ================================================================================================
 
+//  chat search START ================================================================================================
+;(function(){     
+
+        var menu_chat = document.querySelector('.js-list_chat');
+        var list_chat_items = menu_chat.querySelectorAll('.messenger');
+        var chat_input = menu_chat.querySelector('.js-chat_search')
+
+        var list_chat_item_search = [];
+
+        list_chat_items.forEach(function(value,index){
+        list_chat_item_search.push(removeVietnameseTones(value.querySelector('.messenger_content p').innerText.toLowerCase()));
+        });
+
+        chat_input.addEventListener('keyup',function(){
+         var val_input = removeVietnameseTones(chat_input.value.toLowerCase());
+
+        list_chat_item_search.forEach(function(value,index){
+
+            list_chat_items[index].parentNode.classList.add('ds0');
+
+            if(value.indexOf(val_input) !== -1){
+            list_chat_items[index].parentNode.classList.remove('ds0');
+            }
+        });   
+        
+    });
+    
+    })();
 //  chat search END ================================================================================================
 
+// open-close modal-post..... START ========================================
+;(function(){
+     var modal_update = document.querySelector(".js-creat_post");
+     var modal_content = modal_update.querySelector(".upload_content");
+     var btn_close_modal = modal_update.querySelector(".js-close_modal");
+     var btn_open_modal = document.querySelectorAll(".js-open_modal_update");
+
+    function open(){
+        modal_update.classList.add("ds1");
+    }
+    function close(){
+        modal_update.classList.remove("ds1");
+    }
+
+    btn_open_modal.forEach(function(value,index){
+        value.addEventListener('click',open);
+    });
+    btn_close_modal.addEventListener('click',close);
+    modal_update.addEventListener('click',close);
+
+    modal_content.addEventListener('click',function(event){
+        event.stopPropagation();
+    })
+})();
+// open-close modal-post..... END ==========================================
+
+// open-close modal-room..... START ========================================
+
+;(function(){
+    var modal_update = document.querySelector(".js-creat_room");
+    var modal_content = modal_update.querySelector(".upload_content");
+    var btn_close_modal = modal_update.querySelector(".js-close_modal");
+    var btn_open_modal = document.querySelector(".js-open_room");
+
+   function open(){
+       modal_update.classList.add("ds1");
+   }
+   function close(){
+       modal_update.classList.remove("ds1");
+   }
+
+   
+    btn_open_modal.addEventListener('click',open);
+    btn_close_modal.addEventListener('click',close);
+    modal_update.addEventListener('click',close);
+
+    modal_content.addEventListener('click',function(event){
+       event.stopPropagation();
+   })
+})();
+
+// open-close modal-room..... END ==========================================
